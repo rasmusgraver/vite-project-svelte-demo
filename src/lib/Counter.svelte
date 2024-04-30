90f4f85db2e2dacf98284601e346ce4672eb1c28
Rasmus Graver <rasmus.graver@gmail.com>
2024-04-30 11:27:48 +0200

    initial commit

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  let count: number = 0
+  const increment = () => {
+    count += 1
+  }
+</script>
+
+<button on:click={increment}>
+  count is {count}
+</button>
+
+<h2>Also here count is {count}</h2>
